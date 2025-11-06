ee983fb57064bc9dca4db34557e99a5d84fc54b5
alexandrecardososalles15-oss <alexandrecardososalles15@gmail.com>
2025-11-06 14:11:36 -0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,8 +1,25 @@
-diaDaSemana = prompt('Qual é o dia da semana?');
-if (diaDaSemana == 'Sábado') {
-    alert('Bom fim de semana!');
-} else if (diaDaSemana == 'Domingo') {
-    alert('Bom fim de semana!');
-} else {
-    alert('Boa semana!');
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // tentativas = tentativas + 1
+        tentativas++
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
